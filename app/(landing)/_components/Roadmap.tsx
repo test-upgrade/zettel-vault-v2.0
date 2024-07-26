a2eb32da919c19aaffa90dd125ff4bc86a85e1d6
ashwithbash <mohammadquadeer2006@gmail.com>
2024-07-27 02:52:12 +0530

    asd

--- app/(landing)/_components/Roadmap.tsx
+++ app/(landing)/_components/Roadmap.tsx
@@ -7,22 +7,23 @@ import { roadmap } from "../constants";
 import { check2, grid, loading1 } from "../assets";
 import { Gradient } from "./design/Roadmap";
 import Image from "next/image";
+import { StaticImageData } from "next/image";
 
 interface RoadmapItem {
   id: string;
   date: string;
   status: "done" | "progress"; // Updated to match data
   colorful?: boolean;
-  imageUrl: string;
+  imageUrl: StaticImageData;
   title: string;
   text: string;
 }
 
+
+
 const Roadmap: React.FC = () => (
   <Section className="overflow-hidden" id="roadmap">
     <div className="container md:pb-10">
-      {/* <Heading tag="Ready to get started" title="What we’re working on" /> */}
-
       <div className="relative grid gap-6 md:grid-cols-2 md:gap-4 md:pb-[7rem]">
         {roadmap.map((item: RoadmapItem) => {
           const status = item.status === "done" ? "Done" : "In progress";
@@ -46,8 +47,6 @@ const Roadmap: React.FC = () => (
                 </div>
                 <div className="relative z-1">
                   <div className="flex items-center justify-between max-w-[27rem] mb-8 md:mb-20">
-                    {/* <Tagline>{item.date}</Tagline> */}
-
                     <div className="flex items-center px-4 py-1 bg-n-1 rounded text-n-8">
                       <Image
                         className="mr-2.5"
@@ -79,10 +78,6 @@ const Roadmap: React.FC = () => (
 
         <Gradient />
       </div>
-
-      {/* <div className="flex justify-center mt-12 md:mt-15 xl:mt-20">
-        <Button href="/roadmap">Our roadmap</Button>
-      </div> */}
     </div>
   </Section>
 );
