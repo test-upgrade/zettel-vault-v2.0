a1ea9011b1cb02b0070a33571fe8ad4da13c44ce
ashwithbash <mohammadquadeer2006@gmail.com>
2024-07-27 02:47:00 +0530

    a

--- app/(landing)/_components/Hero.tsx
+++ app/(landing)/_components/Hero.tsx
@@ -87,7 +87,7 @@ const Hero = () => {
                   <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
                     {heroIcons.map((icon, index) => (
                       <li className="p-5" key={index}>
-                        <Image src={icon} width={24} height={25} alt={icon} />
+                        <Image src={icon} width={24} height={25} alt="icon" />
                       </li>
                     ))}
                   </ul>
