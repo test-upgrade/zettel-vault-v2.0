f5e0dd4b00bbc68f502d12586dbf0765af236beb
test-upgrade <mohammadquadeer2006@gmail.com>
2024-07-28 22:21:25 +0530

    Update Hero.tsx

--- app/(landing)/_components/Hero.tsx
+++ app/(landing)/_components/Hero.tsx
@@ -30,7 +30,7 @@ const Hero = () => {
       <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
-            Suoercharge Your Productivity With{" "}
+            Supercharge Your Productivity With{" "}
             <span className="inline-block relative">
               Zettel Vault{" "}
               <Image
